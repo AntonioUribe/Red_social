3788b4400d3c4541c170a2600ebaa6b5c249d513
unknown <salazarantoniosa@gmail.com>
2022-02-01 23:38:48 -0600

    con js

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,7 @@
-console.log("hol mundo");
+const navToggle = document.querySelector(".nav-toggle")
+const navMenu = document.querySelector(".nav-menu")
+
+navToggle.addEventListener("click",()=>{
+    navMenu.classList.toggle("nav-menu_visible")
+
+})
