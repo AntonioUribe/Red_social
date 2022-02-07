dff894567757597227bdfd4723614f46e14ddf43
unknown <salazarantoniosa@gmail.com>
2022-02-07 13:03:25 -0600

    Navbar Encabezado

---
+++ JS/app.js
@@ -0,0 +1,8 @@
+
+const navToggle = document.querySelector(".btn_menu")
+const navMenu = document.querySelector(".menu-items")
+
+navToggle.addEventListener("click",()=>{
+    navMenu.classList.toggle("show")
+
+})
---
+++ JS/ezz.js
@@ -0,0 +1,8 @@
+
+const navToggle = document.querySelector(".navbar-toggler")
+const navMenu = document.querySelector(".navbar")
+
+navToggle.addEventListener("click",()=>{
+    navMenu.classList.toggle("button")
+
+})
